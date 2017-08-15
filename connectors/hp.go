a62ea1ee4ecac4500799d21aed6b48cbff2eee2f
Juliano Martinez <juliano@martinez.io>
2017-08-15 16:03:26 +0200

    We needed a better interface to connect to the ilo. The old method would require a few login to extract the data and we know that iLOs suffer when lots of logins happen

--- connectors/hp.go
+++ connectors/hp.go
@@ -1,5 +1,29 @@
 package connectors
 
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"golang.org/x/net/publicsuffix"
+)
+
+var (
+	// ErrBiosNotFound is returned when we are not able to find the server bios version
+	ErrBiosNotFound = errors.New("Bios version not found")
+)
+
 // HpBlade contains the unmarshalled data from the hp chassis
 type HpBlade struct {
 	HpBay       *HpBay   `xml:" BAY,omitempty"`
@@ -109,17 +133,188 @@ type HpNIC struct {
 	Status      string `xml:" STATUS,omitempty"`
 }
 
-// HpFirmware is the struct used to render the data from https://$ip/json/fw_info
+// HpFirmware is the struct used to render the data from https://$ip/json/fw_info, it contains firmware data of the blade
 type HpFirmware struct {
-	HostpwrState     string `json:"hostpwr_state"`
-	InPost           int    `json:"in_post"`
-	AmsReady         string `json:"ams_ready"`
-	DataStateNetwork string `json:"data_state_network"`
-	DataStateStorage string `json:"data_state_storage"`
-	Firmware         []struct {
-		FwIndex   int    `json:"fw_index"`
+	Firmware []struct {
 		FwName    string `json:"fw_name"`
 		FwVersion string `json:"fw_version"`
-		Location  string `json:"Location"`
 	} `json:"firmware"`
+}
+
+// HpProcs is the struct used to render the data from https://$ip/json/proc_info, it contains the processor data
+type HpProcs struct {
+	Processors []struct {
+		ProcName string `json:"proc_name"`
+	} `json:"processors"`
+}
+
+// HpMem is the struct used to render the data from https://$ip/json/mem_info, it contains the ram data
+type HpMem struct {
+	MemTotalMemSize int `json:"mem_total_mem_size"`
+}
+
+// IloReader holds the status and properties of a connection to an iLO device
+type IloReader struct {
+	ip       *string
+	username *string
+	password *string
+	client   *http.Client
+	loginURL *url.URL
+}
+
+// NewIloReader returns a new IloReader ready to be used
+func NewIloReader(ip *string, username *string, password *string) (ilo *IloReader, err error) {
+	u, err := url.Parse(fmt.Sprintf("https://%s/json/login_session", *ip))
+	if err != nil {
+		return nil, err
+	}
+	return &IloReader{ip: ip, username: username, password: password, loginURL: u}, err
+}
+
+// Login initiates the connection to an iLO device
+func (i *IloReader) Login() (err error) {
+	log.WithFields(log.Fields{"step": "Ilo Connection HP", "ip": *i.ip}).Debug("Connecting to iLO")
+
+	data := []byte(fmt.Sprintf("{\"method\":\"login\", \"user_login\":\"%s\", \"password\":\"%s\" }", *i.username, *i.password))
+
+	req, err := http.NewRequest("POST", i.loginURL.String(), bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	tr := &http.Transport{
+		TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
+		DisableKeepAlives: true,
+		Dial: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 10 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 10 * time.Second,
+	}
+
+	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{
+		Timeout:   time.Second * 20,
+		Transport: tr,
+		Jar:       jar,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	io.Copy(ioutil.Discard, resp.Body)
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 404 {
+		return ErrPageNotFound
+	}
+
+	i.client = client
+
+	return err
+}
+
+// Get calls a given json endpoint of the ilo and returns the data
+func (i *IloReader) Get(endpoint string) (payload []byte, err error) {
+	log.WithFields(log.Fields{"step": "Ilo Connection HP", "ip": *i.ip, "endpoint": endpoint}).Debug("Retrieving data from iLO")
+
+	resp, err := i.client.Get(fmt.Sprintf("https://%s/%s", *i.ip, endpoint))
+	if err != nil {
+		return payload, err
+	}
+	defer resp.Body.Close()
+
+	payload, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return payload, err
+	}
+
+	if resp.StatusCode == 404 {
+		return payload, ErrPageNotFound
+	}
+
+	return payload, err
+}
+
+// Memory return the total amount of memory of the server
+func (i *IloReader) Memory() (mem int, err error) {
+	result, err := i.Get("json/mem_info")
+	if err != nil {
+		return mem, err
+	}
+
+	hpMemData := &HpMem{}
+	err = json.Unmarshal(result, hpMemData)
+	if err != nil {
+		return mem, err
+	}
+
+	return hpMemData.MemTotalMemSize / 1024 / 1024, err
+}
+
+// CPU return the cpu type of the server
+func (i *IloReader) CPU() (cpu string, err error) {
+	result, err := i.Get("json/proc_info")
+	if err != nil {
+		return cpu, err
+	}
+
+	hpProcData := &HpProcs{}
+	err = json.Unmarshal(result, hpProcData)
+	if err != nil {
+		return cpu, err
+	}
+
+	return fmt.Sprintf("2 x %s", hpProcData.Processors[0].ProcName), err
+}
+
+// BiosVersion return the current verion of the bios
+func (i *IloReader) BiosVersion() (version string, err error) {
+	result, err := i.Get("json/fw_info")
+	if err != nil {
+		return version, err
+	}
+
+	hpFwData := &HpFirmware{}
+	err = json.Unmarshal(result, hpFwData)
+	if err != nil {
+		return version, err
+	}
+
+	for _, entry := range hpFwData.Firmware {
+		if entry.FwName == "System ROM" {
+			return entry.FwVersion, err
+		}
+	}
+
+	return version, ErrBiosNotFound
+}
+
+// Logout logs out and close the iLo connection
+func (i *IloReader) Logout() (err error) {
+	log.WithFields(log.Fields{"step": "Ilo Connection HP", "ip": *i.ip}).Debug("Logout from iLO")
+
+	data := []byte(`{"method":"logout"}`)
+
+	req, err := http.NewRequest("POST", i.loginURL.String(), bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := i.client.Do(req)
+	if err != nil {
+		return err
+	}
+	io.Copy(ioutil.Discard, resp.Body)
+	defer resp.Body.Close()
+
+	return err
 }
