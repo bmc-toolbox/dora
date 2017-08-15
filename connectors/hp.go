503d922f57892550808ec1344c544250f29e4ae5
Juliano Martinez <juliano@martinez.io>
2017-08-15 14:07:03 +0200

    hp doesnt offer a json for this horrible data structure, lat's not misslead people

--- connectors/hp.go
+++ connectors/hp.go
@@ -2,111 +2,111 @@ package connectors
 
 // HpBlade contains the unmarshalled data from the hp chassis
 type HpBlade struct {
-	HpBay       *HpBay   `xml:" BAY,omitempty" json:"BAY,omitempty"`
-	Bsn         string   `xml:" BSN,omitempty" json:"BSN,omitempty"`
-	MgmtIPAddr  string   `xml:" MGMTIPADDR,omitempty" json:"MGMTIPADDR,omitempty"`
-	MgmtType    string   `xml:" MGMTPN,omitempty" json:"MGMTPN,omitempty"`
-	MgmtVersion string   `xml:" MGMTFWVERSION,omitempty" json:"MGMTFWVERSION,omitempty"`
-	Name        string   `xml:" NAME,omitempty" json:"NAME,omitempty"`
-	HpPower     *HpPower `xml:" POWER,omitempty" json:"POWER,omitempty"`
-	Status      string   `xml:" STATUS,omitempty" json:"STATUS,omitempty"`
-	Spn         string   `xml:" SPN,omitempty" json:"SPN,omitempty"`
-	HpTemps     *HpTemps `xml:" TEMPS,omitempty" json:"TEMPS,omitempty"`
+	HpBay       *HpBay   `xml:" BAY,omitempty"`
+	Bsn         string   `xml:" BSN,omitempty"`
+	MgmtIPAddr  string   `xml:" MGMTIPADDR,omitempty"`
+	MgmtType    string   `xml:" MGMTPN,omitempty"`
+	MgmtVersion string   `xml:" MGMTFWVERSION,omitempty"`
+	Name        string   `xml:" NAME,omitempty"`
+	HpPower     *HpPower `xml:" POWER,omitempty"`
+	Status      string   `xml:" STATUS,omitempty"`
+	Spn         string   `xml:" SPN,omitempty"`
+	HpTemps     *HpTemps `xml:" TEMPS,omitempty"`
 }
 
 // HpBay contains the position of the blade within the chassis
 type HpBay struct {
-	Connection int `xml:" CONNECTION,omitempty" json:"CONNECTION,omitempty"`
+	Connection int `xml:" CONNECTION,omitempty"`
 }
 
 // HpInfra2 is the data retrieved from the chassis xml interface that contains all components
 type HpInfra2 struct {
-	Addr           string          `xml:" ADDR,omitempty" json:"ADDR,omitempty"`
-	HpBlades       *HpBlades       `xml:" BLADES,omitempty" json:"BLADES,omitempty"`
-	HpSwitches     *HpSwitches     `xml:" SWITCHES,omitempty" json:"SWITCHES,omitempty"`
-	HpChassisPower *HpChassisPower `xml:" POWER,omitempty" json:"POWER,omitempty"`
-	Status         string          `xml:" STATUS,omitempty" json:"STATUS,omitempty"`
-	HpTemps        *HpTemps        `xml:" TEMPS,omitempty" json:"TEMPS,omitempty"`
-	EnclSn         string          `xml:" ENCL_SN,omitempty" json:"ENCL_SN,omitempty"`
-	Pn             string          `xml:" PN,omitempty" json:"PN,omitempty"`
-	Encl           string          `xml:" ENCL,omitempty" json:"ENCL,omitempty"`
-	Rack           string          `xml:" RACK,omitempty" json:"RACK,omitempty"`
+	Addr           string          `xml:" ADDR,omitempty"`
+	HpBlades       *HpBlades       `xml:" BLADES,omitempty"`
+	HpSwitches     *HpSwitches     `xml:" SWITCHES,omitempty"`
+	HpChassisPower *HpChassisPower `xml:" POWER,omitempty"`
+	Status         string          `xml:" STATUS,omitempty"`
+	HpTemps        *HpTemps        `xml:" TEMPS,omitempty"`
+	EnclSn         string          `xml:" ENCL_SN,omitempty"`
+	Pn             string          `xml:" PN,omitempty"`
+	Encl           string          `xml:" ENCL,omitempty"`
+	Rack           string          `xml:" RACK,omitempty"`
 }
 
 // HpMP contains the firmware version and the model of the chassis
 type HpMP struct {
-	Sn   string `xml:" SN,omitempty" json:"SN,omitempty"`
-	Fwri string `xml:" FWRI,omitempty" json:"FWRI,omitempty"`
+	Sn   string `xml:" SN,omitempty"`
+	Fwri string `xml:" FWRI,omitempty"`
 }
 
 // HpSwitches contains all the switches we have within the chassis
 type HpSwitches struct {
-	HpSwitch []*HpSwitch `xml:" SWITCH,omitempty" json:"BLADE,omitempty"`
+	HpSwitch []*HpSwitch `xml:" SWITCH,omitempty"`
 }
 
 // HpSwitch contains the type of the switch
 type HpSwitch struct {
-	Spn string `xml:" SPN,omitempty" json:"SPN,omitempty"`
+	Spn string `xml:" SPN,omitempty"`
 }
 
 // HpBlades contains all the blades we have within the chassis
 type HpBlades struct {
-	HpBlade []*HpBlade `xml:" BLADE,omitempty" json:"BLADE,omitempty"`
+	HpBlade []*HpBlade `xml:" BLADE,omitempty"`
 }
 
 // HpPower contains the power information of a blade
 type HpPower struct {
-	PowerConsumed float64 `xml:" POWER_CONSUMED,omitempty" json:"POWER_CONSUMED,omitempty"`
+	PowerConsumed float64 `xml:" POWER_CONSUMED,omitempty"`
 }
 
 // HpChassisPower contains the power information of the chassis
 type HpChassisPower struct {
-	PowerConsumed float64          `xml:" POWER_CONSUMED,omitempty" json:"POWER_CONSUMED,omitempty"`
-	HpPowersupply []*HpPowersupply `xml:" POWERSUPPLY,omitempty" json:"POWERSUPPLY,omitempty"`
+	PowerConsumed float64          `xml:" POWER_CONSUMED,omitempty"`
+	HpPowersupply []*HpPowersupply `xml:" POWERSUPPLY,omitempty"`
 }
 
 // HpRimp is the entry data structure for the chassis
 type HpRimp struct {
-	HpInfra2 *HpInfra2 `xml:" INFRA2,omitempty" json:"INFRA2,omitempty"`
-	HpMP     *HpMP     `xml:" MP,omitempty" json:"MP,omitempty"`
+	HpInfra2 *HpInfra2 `xml:" INFRA2,omitempty"`
+	HpMP     *HpMP     `xml:" MP,omitempty"`
 }
 
 // HpPowersupply contains the data of the power supply of the chassis
 type HpPowersupply struct {
-	Status string `xml:" STATUS,omitempty" json:"STATUS,omitempty"`
+	Status string `xml:" STATUS,omitempty"`
 }
 
 // HpTemp contains the thermal data of a chassis or blade
 type HpTemp struct {
 	C    int    `xml:" C,omitempty" json:"C,omitempty"`
-	Desc string `xml:" DESC,omitempty" json:"DESC,omitempty"`
+	Desc string `xml:" DESC,omitempty"`
 }
 
 // HpTemps contains the thermal data of a chassis or blade
 type HpTemps struct {
-	HpTemp *HpTemp `xml:" TEMP,omitempty" json:"TEMP,omitempty"`
+	HpTemp *HpTemp `xml:" TEMP,omitempty"`
 }
 
 // HpRimpBlade is the entry data structure for the blade when queries directly
 type HpRimpBlade struct {
-	HpHSI *HpHSI `xml:" HSI,omitempty" json:"HSI,omitempty"`
+	HpHSI *HpHSI `xml:" HSI,omitempty"`
 }
 
 // HpHSI contains the information about the components of the blade
 type HpHSI struct {
-	HpNICS *HpNICS `xml:" NICS,omitempty" json:"NICS,omitempty"`
+	HpNICS *HpNICS `xml:" NICS,omitempty"`
 }
 
 // HpNICS contains the list of nics that a blade has
 type HpNICS struct {
-	HpNIC []*HpNIC `xml:" NIC,omitempty" json:"NICS,omitempty"`
+	HpNIC []*HpNIC `xml:" NIC,omitempty"`
 }
 
 // HpNIC contains the nic information of a blade
 type HpNIC struct {
-	Description string `xml:" DESCRIPTION,omitempty" json:"DESCRIPTION,omitempty"`
-	MacAddr     string `xml:" MACADDR,omitempty" json:"MACADDR,omitempty"`
-	Status      string `xml:" STATUS,omitempty" json:"STATUS,omitempty"`
+	Description string `xml:" DESCRIPTION,omitempty"`
+	MacAddr     string `xml:" MACADDR,omitempty"`
+	Status      string `xml:" STATUS,omitempty"`
 }
 
 // HpFirmware is the struct used to render the data from https://$ip/json/fw_info
