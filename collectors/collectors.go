d6455ee30b8a3c2102d65e74ba5d8a71e4541b26
Juliano Martinez <juliano@martinez.io>
2017-09-08 10:16:32 +0200

    commit old old before cleanup

--- collectors/collectors.go
+++ collectors/collectors.go
@@ -1,85 +1,85 @@
 package collectors
 
-import (
-	"strings"
+// import (
+// 	"strings"
 
-	log "github.com/sirupsen/logrus"
-	"gitlab.booking.com/infra/dora/connectors"
-	"gitlab.booking.com/infra/dora/model"
-	"gitlab.booking.com/infra/dora/simpleapi"
-	"gitlab.booking.com/infra/dora/storage"
-)
+// 	log "github.com/sirupsen/logrus"
+// 	"gitlab.booking.com/infra/dora/connectors"
+// 	"gitlab.booking.com/infra/dora/model"
+// 	"gitlab.booking.com/infra/dora/simpleapi"
+// 	"gitlab.booking.com/infra/dora/storage"
+// )
 
-type Collector struct {
-	username    string
-	password    string
-	telegrafURL string
-	simpleAPI   *simpleapi.SimpleAPI
-}
+// type Collector struct {
+// 	username    string
+// 	password    string
+// 	telegrafURL string
+// 	simpleAPI   *simpleapi.SimpleAPI
+// }
 
-type RawCollectedData struct {
-	PowerUsage  string
-	Temperature string
-	Vendor      string
-}
+// type RawCollectedData struct {
+// 	PowerUsage  string
+// 	Temperature string
+// 	Vendor      string
+// }
 
-func (c *Collector) CollectChassis(input <-chan simpleapi.Chassis) {
-	for chassis := range input {
-		rack, err := c.simpleAPI.GetRack(&chassis.Rack)
-		if err != nil {
-			log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis"}).Info("Received errors %s trying to to get data of rack %s\n", err, chassis.Rack)
-			continue
-		}
+// func (c *Collector) CollectChassis(input <-chan simpleapi.Chassis) {
+// 	for chassis := range input {
+// 		rack, err := c.simpleAPI.GetRack(&chassis.Rack)
+// 		if err != nil {
+// 			log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis"}).Info("Received errors %s trying to to get data of rack %s\n", err, chassis.Rack)
+// 			continue
+// 		}
 
-		if rack.Site == "" || rack.Sitezone == "" || rack.Sitepod == "" || rack.Siterow == "" || chassis.Rack == "" {
-			log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis"}).Warning("Position in the datacenter missing")
-		}
+// 		if rack.Site == "" || rack.Sitezone == "" || rack.Sitepod == "" || rack.Siterow == "" || chassis.Rack == "" {
+// 			log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis"}).Warning("Position in the datacenter missing")
+// 		}
 
-		for ifname, ifdata := range chassis.Interfaces {
-			if ifdata.IPAddress == "" {
-				continue
-			}
+// 		for ifname, ifdata := range chassis.Interfaces {
+// 			if ifdata.IPAddress == "" {
+// 				continue
+// 			}
 
-			err := c.collectChassis(&chassis, &rack, &ifdata.IPAddress, &ifname)
-			if err == nil {
-				break
-			} else {
-				log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis", "error": err}).Error("Error collecting chassis data")
-			}
-		}
-	}
-}
+// 			err := c.collectChassis(&chassis, &rack, &ifdata.IPAddress, &ifname)
+// 			if err == nil {
+// 				break
+// 			} else {
+// 				log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis", "error": err}).Error("Error collecting chassis data")
+// 			}
+// 		}
+// 	}
+// }
 
-func (c *Collector) collectChassis(chassis *simpleapi.Chassis, rack *simpleapi.Rack, ip *string, iname *string) (err error) {
-	log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis", "address": *ip, "interface": *iname, "level": "chasssis"}).Info("Collecting data")
+// func (c *Collector) collectChassis(chassis *simpleapi.Chassis, rack *simpleapi.Rack, ip *string, iname *string) (err error) {
+// 	log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis", "address": *ip, "interface": *iname, "level": "chasssis"}).Info("Collecting data")
 
-	conn := connectors.NewChassisConnection(c.username, c.password)
-	var chassisData model.Chassis
-	if strings.HasPrefix(chassis.Model, "BladeSystem") {
-		chassisData, err = conn.Hp(ip)
-		if err != nil {
-			return err
-		}
-	} else if strings.HasPrefix(chassis.Model, "P") {
-		chassisData, err = conn.Dell(ip)
-		if err != nil {
-			return err
-		}
-	} else {
-		log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis", "address": *ip, "interface": *iname, "level": "chasssis", "Error": "Vendor unknown"}).Error("Collecting data")
-	}
+// 	conn := connectors.NewChassisConnection(c.username, c.password)
+// 	var chassisData model.Chassis
+// 	if strings.HasPrefix(chassis.Model, "BladeSystem") {
+// 		chassisData, err = conn.Hp(ip)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	} else if strings.HasPrefix(chassis.Model, "P") {
+// 		chassisData, err = conn.Dell(ip)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	} else {
+// 		log.WithFields(log.Fields{"fqdn": chassis.Fqdn, "type": "chassis", "address": *ip, "interface": *iname, "level": "chasssis", "Error": "Vendor unknown"}).Error("Collecting data")
+// 	}
 
-	db := storage.InitDB()
+// 	db := storage.InitDB()
 
-	chassisStorage := storage.NewChassisStorage(db)
-	_, err = chassisStorage.UpdateOrCreate(&chassisData)
-	if err != nil {
-		return err
-	}
+// 	chassisStorage := storage.NewChassisStorage(db)
+// 	_, err = chassisStorage.UpdateOrCreate(&chassisData)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func New(username string, password string, telegrafURL string, simpleApi *simpleapi.SimpleAPI) *Collector {
-	return &Collector{username: username, password: password, telegrafURL: telegrafURL, simpleAPI: simpleApi}
-}
+// func New(username string, password string, telegrafURL string, simpleApi *simpleapi.SimpleAPI) *Collector {
+// 	return &Collector{username: username, password: password, telegrafURL: telegrafURL, simpleAPI: simpleApi}
+// }
