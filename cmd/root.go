82342aa9132ae4ce9ccaa27743a6fd7b5d8d627a
Juliano Martinez <juliano@martinez.io>
2017-09-17 17:48:43 +0200

    first commit

---
+++ cmd/root.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gitlab.booking.com/infra/nestor/server"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "nestor",
+	Short: "Nestor is a bridge between the ServerDB and kea",
+	Long: `Nestor is a bridge between the ServerDB and kea. Nestor will query the ServerDB and store
+the data using the lru cache to ensure we don't slow down the dhcp handling..
+`,
+	Args: cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		server.Serve()
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/bmc-toolbox/nestor.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".nestor" (without extension).
+		viper.SetConfigName("nestor")
+		viper.AddConfigPath(fmt.Sprintf("%s/.bmc-toolbox", home))
+		viper.AddConfigPath("/etc/bmc-toolbox")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+	viper.SetDefault("notify_url", "https://serverdb.booking.com")
+	viper.SetDefault("socket_path", "/tmp/nestor.sock")
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,128 @@
+package server
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/cookiejar"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
+	"github.com/spf13/viper"
+	"golang.org/x/net/publicsuffix"
+)
+
+var (
+	clru        = cache.New(60*time.Minute, 90*time.Minute)
+	client      *http.Client
+	authHeader  string
+	serverDBUrl string
+	// ErrPageNotFound is used to inform the http request that we couldn't find the expected page and/or endpoint
+	ErrPageNotFound = errors.New("Requested page couldn't be found in the server")
+)
+
+type serverDBResponse struct {
+	IPAddress string `json:"ip_address"`
+	MacAddres string `json:"mac_address"`
+}
+
+func buildClient() (client *http.Client, err error) {
+	tr := &http.Transport{
+		TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
+		DisableKeepAlives: false,
+		Dial: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
+	if err != nil {
+		return client, err
+	}
+
+	client = &http.Client{
+		Timeout:   time.Second * 60,
+		Transport: tr,
+		Jar:       jar,
+	}
+
+	return client, err
+}
+
+func serverDBQuery(subnet *string, macAddress *string) (lease string, err error) {
+	key := fmt.Sprintf("%s-%s", *subnet, *macAddress)
+	data, found := clru.Get(key)
+	if found {
+		return data.(string), err
+	}
+
+	url := fmt.Sprintf("%s/api/v1/interface/dhcp_request/%s/%s/", serverDBUrl, *subnet, *macAddress)
+	fmt.Println(url)
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return lease, err
+	}
+	//req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", authHeader)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return lease, err
+	}
+
+	if resp.StatusCode == 404 {
+		return lease, ErrPageNotFound
+	}
+
+	payload, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return lease, err
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(string(payload))
+
+	sdbr := &serverDBResponse{}
+	err = json.Unmarshal(payload, sdbr)
+	if err != nil {
+		return lease, err
+	}
+
+	clru.Set(key, sdbr.IPAddress, cache.DefaultExpiration)
+	return lease, err
+}
+
+func Serve() {
+	os.Remove(viper.GetString("socket_path"))
+
+	authHeader = fmt.Sprintf("ApiKey %s:%s", viper.GetString("notify_api_user"), viper.GetString("notify_api_key"))
+	serverDBUrl = viper.GetString("notify_url")
+
+	var err error
+	client, err = buildClient()
+	if err != nil {
+		panic(err)
+	}
+
+	router := gin.Default()
+
+	router.GET("/kea/:subnet/:macAddress", func(c *gin.Context) {
+		subnet := c.Param("subnet")
+		macAddress := c.Param("macAddress")
+		//data := fmt.Sprintf(subnet, macAddress)
+		data, err := serverDBQuery(&subnet, &macAddress)
+		if err != nil {
+			c.String(http.StatusForbidden, fmt.Sprintf("We got an error from ServerDB %s", err))
+		}
+		c.String(http.StatusOK, data)
+	})
+
+	router.RunUnix(viper.GetString("socket_path"))
+}
