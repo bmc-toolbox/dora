56bcf70ff2883494f40e518e304044ef9ae230de
Juliano Martinez <juliano@martinez.io>
2017-09-18 13:18:39 +0200

    Implement endpoint for metrics with Cache Miss and Hits

--- server/server.go
+++ server/server.go
@@ -62,6 +62,10 @@ func serverDBQuery(subnet *string, macAddress *string) (lease *string, err error
 	data, found := clru.Get(key)
 	if found {
 		lease := data.(*string)
+		_, err := clru.IncrementInt("CacheHit", 1)
+		if err != nil {
+			log.Println(err)
+		}
 		return lease, err
 	}
 
@@ -94,17 +98,42 @@ func serverDBQuery(subnet *string, macAddress *string) (lease *string, err error
 	}
 
 	clru.Set(key, &sdbr.IPAddress, cache.DefaultExpiration)
+	_, err = clru.IncrementInt("CacheMiss", 1)
+	if err != nil {
+		log.Println(err)
+	}
+
 	return &sdbr.IPAddress, err
 }
 
+// cacheStats returns that status of our cache
+func cacheStats() (hit int, miss int, ratio float64) {
+	if data, found := clru.Get("CacheHit"); found {
+		hit = data.(int)
+	}
+	if data, found := clru.Get("CacheMiss"); found {
+		miss = data.(int)
+	}
+	ratio = float64(hit) / (float64(hit) + float64(miss))
+	return hit, miss, ratio
+}
+
+// Serve start and build the webservice binding on unix socket
 func Serve() {
 	os.Remove(viper.GetString("socket_path"))
+	var err error
+	cacheFile := viper.GetString("cache_file")
+
+	if _, err := os.Stat(cacheFile); !os.IsNotExist(err) {
+		err := clru.LoadFile(cacheFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	authHeader = fmt.Sprintf("ApiKey %s:%s", viper.GetString("notify_api_user"), viper.GetString("notify_api_key"))
 	serverDBUrl = viper.GetString("notify_url")
 	debug := viper.GetBool("debug")
-
-	var err error
 
 	client, err = buildClient()
 	if err != nil {
@@ -115,7 +144,21 @@ func Serve() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
+	go func(clru *cache.Cache, cacheFile *string) {
+		for {
+			time.Sleep(1 * time.Minute)
+			err := clru.SaveFile(*cacheFile)
+			if err != nil {
+				log.Println("Error saving cache file: ", err)
+			} else {
+				log.Println("Cache file saved: ", *cacheFile)
+			}
+		}
+	}(clru, &cacheFile)
+
 	router := gin.Default()
+	clru.Set("CacheHit", 0, cache.NoExpiration)
+	clru.Set("CacheMiss", 0, cache.NoExpiration)
 
 	router.GET("/kea/:subnet/:macAddress/", func(c *gin.Context) {
 		subnet := c.Param("subnet")
@@ -129,6 +172,15 @@ func Serve() {
 		if debug {
 			log.Printf("Sending answer from %s - %s: %s", subnet, macAddress, *data)
 		}
+	})
+
+	router.GET("/stats/", func(c *gin.Context) {
+		hit, miss, ratio := cacheStats()
+		c.JSON(http.StatusOK, gin.H{
+			"CacheHit":   hit,
+			"CacheMiss":  miss,
+			"CacheRatio": ratio,
+		})
 	})
 
 	router.RunUnix(viper.GetString("socket_path"))
