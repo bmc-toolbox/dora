c02effcb334db9426f614ce711e52d6bb50ad99d
Juliano Martinez <juliano@martinez.io>
2017-10-26 15:37:16 +0200

    adds Nics as part of the chassis bmc interface

--- connectors/interfaces.go
+++ connectors/interfaces.go
@@ -4,34 +4,35 @@ import "gitlab.booking.com/infra/dora/model"
 
 // Bmc represents the requirement of items to be collected a server
 type Bmc interface {
-	Serial() (string, error)
-	Model() (string, error)
+	BiosVersion() (string, error)
 	BmcType() (string, error)
 	BmcVersion() (string, error)
-	Name() (string, error)
-	Status() (string, error)
-	Memory() (int, error)
 	CPU() (string, int, int, int, error)
-	BiosVersion() (string, error)
-	PowerKw() (float64, error)
-	TempC() (int, error)
-	Nics() ([]*model.Nic, error)
 	License() (string, string, error)
 	Login() error
 	Logout() error
+	Memory() (int, error)
+	Model() (string, error)
+	Name() (string, error)
+	Nics() ([]*model.Nic, error)
+	PowerKw() (float64, error)
+	Serial() (string, error)
+	Status() (string, error)
+	TempC() (int, error)
 }
 
 // BmcChassis represents the requirement of items to be collected from a chassis
 type BmcChassis interface {
-	Name() (string, error)
-	Model() (string, error)
-	Serial() (string, error)
-	PowerKw() (float64, error)
-	TempC() (int, error)
-	Status() (string, error)
-	FwVersion() (string, error)
-	PassThru() (string, error)
-	PowerSupplyCount() (int, error)
 	Blades() ([]*model.Blade, error)
+	FwVersion() (string, error)
+	Model() (string, error)
+	Name() (string, error)
+	Nics() ([]*model.Nic, error)
+	PassThru() (string, error)
+	PowerKw() (float64, error)
+	PowerSupplyCount() (int, error)
+	Serial() (string, error)
+	Status() (string, error)
 	StorageBlades() ([]*model.StorageBlade, error)
+	TempC() (int, error)
 }
