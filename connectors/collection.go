05c83608996246eac4a24c3a835138ce0f12a5be
Juliano Martinez <juliano@martinez.io>
2017-11-29 11:36:59 +0100

    add support for default password for vendor if required

--- connectors/collection.go
+++ connectors/collection.go
@@ -3,6 +3,7 @@ package connectors
 import (
 	"fmt"
 	"net"
+	"strings"
 	"sync"
 
 	"github.com/jinzhu/gorm"
@@ -23,19 +24,29 @@ func collect(input <-chan string, source *string, db *gorm.DB) {
 	for host := range input {
 		c, err := NewConnection(bmcUser, bmcPass, host)
 		if err != nil {
-			log.WithFields(log.Fields{"operation": "connection", "ip": host, "type": c.HwType(), "error": err}).Error("connecting to host")
+			log.WithFields(log.Fields{"operation": "connection", "ip": host, "type": c.HwType()}).Error(err)
 			continue
 		}
 
 		if c.HwType() == Blade && *source != "cli-with-force" {
-			log.WithFields(log.Fields{"operation": "connection", "ip": host, "type": c.HwType()}).Debug("we don't want to scan blades directly since the chassis does it for us")
+			log.WithFields(log.Fields{"operation": "detection", "ip": host, "type": c.HwType()}).Debug("we don't want to scan blades directly since the chassis does it for us")
 			continue
 		}
 
 		data, err := c.Collect()
 		if err != nil {
-			log.WithFields(log.Fields{"operation": "connection", "ip": host, "type": c.HwType(), "error": err}).Error("collecting data")
-			continue
+			log.WithFields(log.Fields{"operation": "collection", "ip": host, "type": c.HwType()}).Error(err)
+			if err == ErrLoginFailed && viper.GetBool("collector.try_default_credentials") {
+				c.username = viper.GetString(fmt.Sprintf("collector.default.%s.username", strings.ToLower(c.Vendor())))
+				c.password = viper.GetString(fmt.Sprintf("collector.default.%s.password", strings.ToLower(c.Vendor())))
+				data, err = c.Collect()
+				if err != nil {
+					log.WithFields(log.Fields{"operation": "connection", "ip": host, "type": c.HwType(), "error": err}).Error("collecting data")
+					continue
+				}
+			} else {
+				continue
+			}
 		}
 
 		switch data.(type) {
@@ -48,13 +59,13 @@ func collect(input <-chan string, source *string, db *gorm.DB) {
 			chassisStorage := storage.NewChassisStorage(db)
 			existingData, err := chassisStorage.GetOne(chassis.Serial)
 			if err != nil && err != gorm.ErrRecordNotFound {
-				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType(), "error": err}).Error("collecting data")
+				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType()}).Error(err)
 				continue
 			}
 
 			_, err = chassisStorage.UpdateOrCreate(chassis)
 			if err != nil {
-				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType(), "error": err}).Error("collecting data")
+				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType()}).Error(err)
 				continue
 			}
 
@@ -73,13 +84,13 @@ func collect(input <-chan string, source *string, db *gorm.DB) {
 			bladeStorage := storage.NewBladeStorage(db)
 			existingData, err := bladeStorage.GetOne(blade.Serial)
 			if err != nil && err != gorm.ErrRecordNotFound {
-				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType(), "error": err}).Error("collecting data")
+				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType()}).Error(err)
 				continue
 			}
 
 			_, err = bladeStorage.UpdateOrCreate(blade)
 			if err != nil {
-				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType(), "error": err}).Error("collecting data")
+				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType()}).Error(err)
 				continue
 			}
 
@@ -98,13 +109,13 @@ func collect(input <-chan string, source *string, db *gorm.DB) {
 			discreteStorage := storage.NewDiscreteStorage(db)
 			existingData, err := discreteStorage.GetOne(discrete.Serial)
 			if err != nil && err != gorm.ErrRecordNotFound {
-				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType(), "error": err}).Error("collecting data")
+				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType()}).Error(err)
 				continue
 			}
 
 			_, err = discreteStorage.UpdateOrCreate(discrete)
 			if err != nil {
-				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType(), "error": err}).Error("collecting data")
+				log.WithFields(log.Fields{"operation": "store", "ip": host, "type": c.HwType()}).Error(err)
 				continue
 			}
 
@@ -139,7 +150,7 @@ func DataCollection(ips []string, source string) {
 		for callback := range notification {
 			err := assetNotify(callback)
 			if err != nil {
-				log.WithFields(log.Fields{"operation": "ServerDB callback", "url": callback, "error": err}).Error("sending ServerDB callback")
+				log.WithFields(log.Fields{"operation": "ServerDB callback", "url": callback}).Error(err)
 			}
 		}
 	}(notifyChange)
@@ -147,7 +158,7 @@ func DataCollection(ips []string, source string) {
 	if ips[0] == "all" {
 		hosts := []model.ScannedPort{}
 		if err := db.Where("port = 443 and protocol = 'tcp' and state = 'open'").Find(&hosts).Error; err != nil {
-			log.WithFields(log.Fields{"operation": "connection", "ip": "all", "error": err}).Error(fmt.Sprintf("retrieving scanned hosts"))
+			log.WithFields(log.Fields{"operation": "retrieving scanned hosts", "ip": "all"}).Error(err)
 		} else {
 			for _, host := range hosts {
 				cc <- host.IP
@@ -160,14 +171,14 @@ func DataCollection(ips []string, source string) {
 			if parsedIP == nil {
 				lookup, err := net.LookupHost(ip)
 				if err != nil {
-					log.WithFields(log.Fields{"operation": "connection", "ip": ip, "error": err}).Error(fmt.Sprintf("retrieving scanned hosts"))
+					log.WithFields(log.Fields{"operation": "retrieving scanned hosts", "ip": ip}).Error(err)
 					continue
 				}
 				ip = lookup[0]
 			}
 
 			if err := db.Where("ip = ? and port = 443 and protocol = 'tcp' and state = 'open'", ip).Find(&host).Error; err != nil {
-				log.WithFields(log.Fields{"operation": "connection", "ip": ip, "error": err}).Error(fmt.Sprintf("retrieving scanned hosts"))
+				log.WithFields(log.Fields{"operation": "retrieving scanned hosts", "ip": ip}).Error(err)
 				continue
 			}
 			cc <- host.IP
