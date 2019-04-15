e860e7aa2f3d0baae246a9f28dc79733cb7c2685
Dmitry Verkhoturov <dmitry.verkhoturov@booking.com>
2019-04-15 12:06:27 +0200

    add filter tests

---
+++ filter/filter_test.go
@@ -0,0 +1,36 @@
+package filter
+
+import (
+	"github.com/bmc-toolbox/dora/model"
+	"github.com/manyminds/api2go"
+	"github.com/stretchr/testify/assert"
+	"net/url"
+	"testing"
+)
+
+var testSet = []struct {
+	urlString string
+	sqlQuery  string
+}{
+	{"filter[model]=dell", "model in ('dell')"},
+	{"filter[status]!=bad", "status not in ('bad')"},
+}
+
+func TestEqualSignAndExclamationMark(t *testing.T) {
+	for _, testPair := range testSet {
+		queryParams, _ := url.ParseQuery(testPair.urlString)
+		request := api2go.Request{
+			QueryParams: queryParams,
+		}
+		filters, hasFilters := NewFilterSet(&request)
+
+		assert.EqualValues(t, true, hasFilters,
+			"filter is created")
+
+		query, err := filters.BuildQuery(model.Chassis{})
+		if err != nil {
+			panic(err)
+		}
+		assert.Equal(t, testPair.sqlQuery, query)
+	}
+}
